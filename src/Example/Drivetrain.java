94359effa82518bd9b5a3b01461c75435fe32a09
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-08-17 15:01:32 -0400

    Update Drivetrain.java

--- src/Example/Drivetrain.java
+++ src/Example/Drivetrain.java
@@ -1,7 +1,9 @@
 package Example;
 
 import Example.Vector2D
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+// This code serves as a base class for a holonomic, mecanum-style drivetrain
 public class Drivetrain
 {
 	// Declare Drive Motors and Array
@@ -9,7 +11,8 @@ public class Drivetrain
 	private DcMotor[] drivers;
   
   /* Constructor for the Drivetrain class.
-   * Pass a hardware map as the first parameter and an array of 4 booleans for the second parameter
+   * @param hwMap       a hardware map 
+	 * @param inversion   array of 4 booleans
    * Motor order for the second parameter is LF, RF, LR, RR. TRUE = FORWARD and FALSE = REVERSE */
   public Drivetrain(HardwareMap hwMap, bool[] inversion) {
     
@@ -51,43 +54,69 @@ public class Drivetrain
    tankDrive(0d, 0d, 0d, 0d); 
   }
 	
-	/* Small utility function that clips a double between two values */
+  /* Small utility function that clips a double between two values */
+	private double clipRange(double x, double min, double max) {
+		return (x < min ? min : x) > max ? max : x;
 	
-	
+	/* Slightly less small utility function to normalize a double array*/
+	protected void normalize(double[] wheelSpeeds, double magnitude) {
+		// The variable maxMagnitude signifies the largest absolute value in the array
+		double maxMagnitude = Math.abs(wheelSpeeds[0]);
+		for (int i = 1; i < wheelSpeeds.length; i++) {
+				double temp = Math.abs(wheelSpeeds[i]);
+				if (maxMagnitude < temp) {
+						maxMagnitude = temp;
+				}
+		}
+
+		// Normalize each unit in the array with the maxMagnitude
+		for (int i = 0; i < wheelSpeeds.length; i++) {
+				wheelSpeeds[i] = (wheelSpeeds[i] / maxMagnitude) * magnitude;
+		}
+	}	
+		
+	/* This program computes the neccessary movement vectors to move the robot in a field-centric manner
+	 *
+	 * @param strafeSpeed  the horizontal speed of the robot, derived from input
+	 * @param forwardSpeed the vertical speed of the robot, derived from input
+	 * @param turnSpeed    the turn speed of the robot, derived from input
+	 * @param gyroAngle    the heading of the robot, derived from the gyro 
+	 */	
   public void driveFieldCentric(double x, double y, double turnSpeed, double gyroAngle) {
-        strafeSpeed = clipRange(strafeSpeed);
-        forwardSpeed = clipRange(forwardSpeed);
-        turnSpeed = clipRange(turnSpeed);
+		// Ensure that x, y, and turnSpeed are between -1 and 1
+		x = clipRange(x, -1d, 1d);
+		y = clipRange(y, -1d, 1d);
+		turnSpeed = clipRange(turnSpeed, -1d, 1d);
+		
+		// Create a new local Vector2D and rotate it so that it is field-centric
+		Vector2d input = new Vector2d(x, y);
+		input = input.rotateBy(-gyroAngle);
 
-        Vector2d input = new Vector2d(strafeSpeed, forwardSpeed);
-        input = input.rotateBy(-gyroAngle);
+		// Get the angle of the resultant vector
+		double theta = input.angle();
 
-        double theta = input.angle();
+		// Calculate wheel speeds for lateral movement only (no rotation)
+		double[] wheelSpeeds = new double[4];
+		wheelSpeeds[MotorType.kFrontLeft.value] = Math.sin(theta + Math.PI / 4);
+		wheelSpeeds[MotorType.kFrontRight.value] = Math.sin(theta - Math.PI / 4);
+		wheelSpeeds[MotorType.kBackLeft.value] = Math.sin(theta - Math.PI / 4);
+		wheelSpeeds[MotorType.kBackRight.value] = Math.sin(theta + Math.PI / 4);
 
-        double[] wheelSpeeds = new double[4];
-        wheelSpeeds[MotorType.kFrontLeft.value] = Math.sin(theta + Math.PI / 4);
-        wheelSpeeds[MotorType.kFrontRight.value] = Math.sin(theta - Math.PI / 4);
-        wheelSpeeds[MotorType.kBackLeft.value] = Math.sin(theta - Math.PI / 4);
-        wheelSpeeds[MotorType.kBackRight.value] = Math.sin(theta + Math.PI / 4);
+		// Normalize data
+		normalize(wheelSpeeds, input.magnitude());
 
-        normalize(wheelSpeeds, input.magnitude());
+		// Factor in turning
+		wheelSpeeds[MotorType.kFrontLeft.value] += turnSpeed;
+		wheelSpeeds[MotorType.kFrontRight.value] -= turnSpeed;
+		wheelSpeeds[MotorType.kBackLeft.value] += turnSpeed;
+		wheelSpeeds[MotorType.kBackRight.value] -= turnSpeed;
 
-        wheelSpeeds[MotorType.kFrontLeft.value] += turnSpeed;
-        wheelSpeeds[MotorType.kFrontRight.value] -= turnSpeed;
-        wheelSpeeds[MotorType.kBackLeft.value] += turnSpeed;
-        wheelSpeeds[MotorType.kBackRight.value] -= turnSpeed;
-
-        normalize(wheelSpeeds);
-
-        motors[MotorType.kFrontLeft.value]
-                .set(wheelSpeeds[MotorType.kFrontLeft.value] * maxOutput);
-        motors[MotorType.kFrontRight.value]
-                .set(wheelSpeeds[MotorType.kFrontRight.value] * rightSideMultiplier * maxOutput);
-        motors[MotorType.kBackLeft.value]
-                .set(wheelSpeeds[MotorType.kBackLeft.value] * maxOutput);
-        motors[MotorType.kBackRight.value]
-                .set(wheelSpeeds[MotorType.kBackRight.value] * rightSideMultiplier * maxOutput);
-    }
+		// Re-Normalize data
+		normalize(wheelSpeeds);
+	  
+		// Apply Movement
+		tankDrive(wheelSpeeds);
+  }
 	
 	  
 }
