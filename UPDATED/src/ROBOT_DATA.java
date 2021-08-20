5da33930f71ebbbc39e97b04e1c0e39eb6f0163a
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-08-20 18:40:06 -0400

    Update ROBOT_DATA.java

--- UPDATED/src/ROBOT_DATA.java
+++ UPDATED/src/ROBOT_DATA.java
@@ -17,6 +17,18 @@ public static class ROBOT_DATA
   static int m_RIGHT_ENCODER_OFFSET = 0;
   static int m_BACK_ENCODER_OFFSET = 0;
   
+  // Encoder positioning
+  static const double TRACK_WIDTH = 5;   // Distance between the 2 vertical encoders
+  
+  // Robot positioning
+  static double m_X = 0;
+  static double m_Y = 0;
+  static double m_THETA = 0;  // This value will only be between 0 and 2pi.
+  
+  // Drive to point tuning values
+  static const DRIVE_TO_POINT_TURN_TUNE = 1;
+  
+
   
   
 }
