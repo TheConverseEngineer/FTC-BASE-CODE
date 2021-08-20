66460bf5d89b0796eada06056fa5a1ef6727be35
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-08-20 16:41:23 -0400

    Create ROBOT_DATA.java

---
+++ UPDATED/src/ROBOT_DATA.java
@@ -0,0 +1,22 @@
+package UPDATED.src;
+
+public static class ROBOT_DATA
+{
+  
+  // Hardware map names
+  static const String LEFT_FRONT_NAME = "leftFrontDrive";
+  static const String = "rightFrontDrive";
+  static const String LEFT_REAR_NAME = "leftRearDrive";
+  static const String RIGHT_REAR_NAME = "rightRearDrive";
+  
+  // Drivetrain inversions
+  static const bool[] MOTOR_INVERTED = {false, true, false, true};
+  
+  // Encoder offsets
+  static int m_LEFT_ENCODER_OFFSET = 0;
+  static int m_RIGHT_ENCODER_OFFSET = 0;
+  static int m_BACK_ENCODER_OFFSET = 0;
+  
+  
+  
+}
