1be147d022316543dd4fcceeae4a17003f991f4a
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-08-21 17:58:20 -0400

    Create PathState.java

---
+++ UPDATED/src/path/PathState.java
@@ -0,0 +1,18 @@
+package UPDATED.src.path;
+
+import UPDATED.src.geometry.Point;
+
+public class PathState
+{
+  
+  public Point point;
+  public int stage;
+    
+    
+  public PathState(Point p, int _stage) {
+    point = p;
+    stage = _stage;
+  }
+  
+  
+}
