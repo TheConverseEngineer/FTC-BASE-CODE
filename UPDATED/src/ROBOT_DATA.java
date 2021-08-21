b1521b36bcc6f4dae36cb81844cba4a02ba90eaf
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-08-21 17:54:07 -0400

    Update ROBOT_DATA.java

--- UPDATED/src/ROBOT_DATA.java
+++ UPDATED/src/ROBOT_DATA.java
@@ -15,19 +15,25 @@ public static class ROBOT_DATA
   static const bool[] MOTOR_INVERTED = {false, true, false, true};
   
   // Encoder positioning and info
-  static const double COUNTS_PER_ROTATIONS = 2048;
-  static const double TRACK_WIDTH_INCHES = 10;                                  // Distance between the 2 vertical encoders
-  static const double TRACK_WIDTH = COUNTS_PER_ROTATION * TRACK_WIDTH_INCHES;   // DO NOT EDIT
+  static const double COUNTS_PER_ROTATIONS = 2048d;
+  static const double DEAD_WHEEL_CIRCUMFRENCE_INCHES = 2.5d;
+  static const double TRACK_WIDTH_INCHES = 10d;                                                    // Distance between the 2 vertical encoders
+  static const double COUNTS_PER_INCH = COUNTS_PER_ROTATIONS * DEAD_WHEEL_CIRCUMFRENCE_INCHES      // DO NOT EDIT
+  static const double TRACK_WIDTH = COUNTS_PER_INCH * TRACK_WIDTH_INCHES;                          // DO NOT EDIT
+  
   
   // Drive to point tuning values
-  static const DRIVE_TO_POINT_TURN_TUNE = 1;
+  static const double DRIVE_TO_POINT_TURN_TUNE = 1d;
+  static const double DEFAULT_LINE_FOLLOW_RANGE_INCHES = 5d;
+  static const double DEFAULT_LINE_FOLLOW_RANGE = DEFAULT_LINE_FOLLOW_RANGE_INCHES * COUNTS_PER_INCH;  // DO NOT EDIT
+  
   
   /****************************** STATES - DO NOT EDIT ******************************/
   
   // Robot positioning
-  static double m_X = 0;
-  static double m_Y = 0;
-  static double m_THETA = 0;  // This value will only be between 0 and 2pi.
+  static double m_X = 0d;
+  static double m_Y = 0d;
+  static double m_THETA = 0d;  // This value will only be between 0 and 2pi.
   
   // Encoder offsets
   static int m_LEFT_ENCODER_OFFSET = 0;
