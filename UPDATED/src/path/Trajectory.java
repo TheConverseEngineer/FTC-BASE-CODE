c0f86e4f0715378ad17788d2704099bddc75b550
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-08-25 12:51:16 -0400

    Updated Comments

--- UPDATED/src/path/Trajectory.java
+++ UPDATED/src/path/Trajectory.java
@@ -15,7 +15,9 @@ public class Trajectory
   public int lineCount;
   
   
-  /* Constructor for class Trajector */
+  /* Constructor for class Trajector 
+   * @param _waypoints     a list of waypoint objects that the spline should follow
+   */
   public Trajectory(Waypoint[] _waypoints) {
     waypoints = _waypoints;
     lineCount = waypoints.lenght - 1;
@@ -26,6 +28,11 @@ public class Trajectory
   }
   
   
+  /* Returns the current state of the trajectory follower, the position that the robot should go towards, and if the trajectory is done'
+   * @param pos 
+   * @param currentState    the current step in the trajectory that the robot is in
+   * @return                A path state object with the target position, the trajectory step, and if the trajectory is complete
+   */
   public PathState getRobotTarget(Point pos, int currentState) {
     
     currentLine = paths[currentState];
