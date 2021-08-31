83a40f77a8a3719679ae0814fa32226899da44c2
jvedock <janekvedock@comcast.net>
2021-08-31 12:33:48 -0400

    added FakeHardwareDevice and FakeDcMotor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team7786/testing/fakes/util/FakeHardwareDevice.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.team7786.testing.fakes.util;
+
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+
+public class FakeHardwareDevice implements HardwareDevice{
+    @Override
+    public Manufacturer getManufacturer(){
+        return null;
+    }
+    @Override
+    public String getDeviceName(){
+        return null;
+    }
+    @Override
+    public String getConnectionInfo(){
+        return null;
+    }
+    @Override
+    public int getVersion(){
+        return 0;
+    }
+    @Override
+    public void resetDeviceConfigurationForOpMode(){
+
+    }
+    @Override
+    public void close(){
+
+    }
+}
---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/team7786/RobotTest.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.team7786;
+
+import junit.framework.TestCase;
+
+public class RobotTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+}
