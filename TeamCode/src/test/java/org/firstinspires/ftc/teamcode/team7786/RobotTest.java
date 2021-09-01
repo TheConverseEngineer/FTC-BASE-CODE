1acffb41981a7d3b5361d8dd58801ba234add459
jvedock <janekvedock@comcast.net>
2021-09-01 07:24:27 -0400

    still more commit stuff i gotta add

--- TeamCode/src/test/java/org/firstinspires/ftc/teamcode/team7786/RobotTest.java
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/team7786/RobotTest.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.team7786;
 
 import junit.framework.TestCase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class RobotTest extends TestCase {
 
+    private HardwareMap testMap;
     public void setUp() throws Exception {
         super.setUp();
+        //testMap.put("leftFrontDrive", DcMotor.class);
+
     }
 }
