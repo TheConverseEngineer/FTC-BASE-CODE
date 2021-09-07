d0c3d9657c15df668cdff4c66b86aff231f2f05b
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-09-07 07:29:07 -0400

    Update and rename TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team7786/TeleGUI/GUI_STATS.java to TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team7786/teleGUI/GUI_Stats.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team7786/teleGUI/GUI_Stats.java
@@ -0,0 +1,13 @@
+//IDK what pacakge this is
+
+
+static class GUI_Stats
+{
+  private static final String[][] TeleGUIArray = new String[][]{{"Alliance", "OPT", "Red", "Blue"},
+                                                                {"Side", "OPT", "Right", "Left"},
+                                                                {"Delay", "INT", "0", "20"},
+                                                                {"Mode", "OPT", "Aggressive", "Passive"}};
+  
+  
+  
+}
