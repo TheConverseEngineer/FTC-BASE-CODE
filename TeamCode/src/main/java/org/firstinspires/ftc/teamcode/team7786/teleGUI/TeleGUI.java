45e4b63112cb4db88363ea700495b40aca517e54
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-09-07 08:30:20 -0400

    Create TeleGUI.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team7786/teleGUI/TeleGUI.java
@@ -0,0 +1,51 @@
+// I still don't know what package this is
+
+// I don't even know what import the other static class is
+// Idk the other imports either
+
+public class TeleGUI {
+  private String[][] strMap;
+  private int[] currentSelected;
+  
+  private List<telemetry.Item> guiLines;
+  private int selected;
+  
+  private static final String START_SELECTED = "◂";
+  private static final String END_SELECTED   = "▸";
+  
+  public TeleGUI(String[][] _map) {
+    strMap = _map; 
+    selected = 0;
+    currentSelected = new int[strMap.length];
+    createGUI();
+  }  
+  
+  private void createGUI () {
+    telemetry.clearAll();
+    
+    for (int i = 0; i < strMap.length; i++) {
+      telemetry.Item genItem = telemetry.AddData(map[i][0], fmtData(i, 2));
+      guiLines.add(genItem);
+      currentSelected[i] = 0;
+    }
+    
+    telemetry.update();
+  }
+  
+  private String fmtData (int line, int opt) {
+    if (selected == line) {
+      return (START_SELECTED + strMap[line][opt] + END_SELECTED); 
+    } else if (selected == line) {
+      return (" " + strMap[line][opt] + " "); 
+    }
+  }
+  
+  private void updateGUI () {
+   for (int i = 0; i < strMap.lenght; i++) {
+     guiLines[i].setValue(fmtData(i, currentSelected[i] + 2);
+   }
+   telemetry.update;
+  }
+  
+  
+}
