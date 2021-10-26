d5524c8aaeba4cd9a15e3bf36ea2834bcb8d615f
The Converse Engineer <58668812+TheConverseEngineer@users.noreply.github.com>
2021-10-26 08:07:42 -0400

    Create MecanumDrive.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team7786/drive/MecanumDrive.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.team7786.drive;
+
+abstract class MecanumDrive {
+  
+  public MecanumDrive(double kV, double kA, double kStatic, double trackWidth, double lateralMultiplier) {
+    
+  }
+  
+  public class MecanumLocalizer {
+    
+    public MecanumLocalizer(MecanumDrive drive, boolean useExternalHeading) {
+      
+    } 
+  }
+  
+}
